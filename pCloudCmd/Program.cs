85437f4344699277cfe400d0fc74b0f909293cf4
SunriseChen <sunrise0918@gmail.com>
2015-12-16 16:16:06 +0800

    创建个人云项目，实现拆分处理。

---
+++ pCloudCmd/Program.cs
@@ -0,0 +1,147 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright company="Sunrise" file="Program.cs">
+//   Copyright (c) Sunrise
+//   All rights reserved.
+// </copyright>
+// <summary>
+//   主程序入口类。
+// </summary>
+// 
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace PersonalCloud.Command
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
+    /// <summary>
+    /// 主程序入口类。
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// 主程序入口。
+        /// </summary>
+        /// <param name="args">命令行参数。</param>
+        public static void Main(string[] args)
+        {
+            OutputArgs(args);
+
+            if (string.Compare(args[0], "s", StringComparison.InvariantCultureIgnoreCase) == 0)
+            {
+                // 拆分文件。
+                SplitFile(args);
+            }
+            else if (string.Compare(args[0], "j", StringComparison.InvariantCultureIgnoreCase) == 0)
+            {
+                // 合并文件。
+                Console.WriteLine("Join");
+            }
+            else if (string.Compare(args[0], "c", StringComparison.InvariantCultureIgnoreCase) == 0)
+            {
+                // 验证文件。
+                Console.WriteLine("Check");
+            }
+            else if (string.Compare(args[0], "u", StringComparison.InvariantCultureIgnoreCase) == 0)
+            {
+                // 上传文件。
+                Console.WriteLine("Upload");
+            }
+            else if (string.Compare(args[0], "d", StringComparison.InvariantCultureIgnoreCase) == 0)
+            {
+                // 下载文件。
+                Console.WriteLine("Download");
+            }
+
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// 获取大小字符串的数值。
+        /// </summary>
+        /// <param name="size">大小字符串。</param>
+        /// <returns>数值。</returns>
+        private static long GetValue(string size)
+        {
+            var valueString = size.Substring(0, size.Length - 1);
+            if (size.EndsWith("b", StringComparison.InvariantCultureIgnoreCase))
+            {
+                size = size.Substring(0, size.Length - 1);
+                valueString = size.Substring(0, size.Length - 1);
+            }
+
+            if (char.IsDigit(size[size.Length - 1]))
+            {
+                valueString = size;
+            }
+
+            long value;
+            if (!long.TryParse(valueString, out value))
+            {
+                return 0L;
+            }
+
+            const long OneUnit = 1024L;
+            if (size.EndsWith("k", StringComparison.InvariantCultureIgnoreCase))
+            {
+                return value * OneUnit;
+            }
+
+            if (size.EndsWith("m", StringComparison.InvariantCultureIgnoreCase))
+            {
+                return value * OneUnit * OneUnit;
+            }
+
+            if (size.EndsWith("g", StringComparison.InvariantCultureIgnoreCase))
+            {
+                return value * OneUnit * OneUnit * OneUnit;
+            }
+
+            if (size.EndsWith("t", StringComparison.InvariantCultureIgnoreCase))
+            {
+                return value * OneUnit * OneUnit * OneUnit * OneUnit;
+            }
+
+            if (size.EndsWith("p", StringComparison.InvariantCultureIgnoreCase))
+            {
+                return value * OneUnit * OneUnit * OneUnit * OneUnit * OneUnit;
+            }
+
+            return value;
+        }
+
+        /// <summary>
+        /// 拆分文件。
+        /// </summary>
+        /// <param name="args">命令行参数。</param>
+        private static void SplitFile(string[] args)
+        {
+            var path = args[1];
+            var size = GetValue(args[2]);
+
+            if (File.Exists(path))
+            {
+                var split = new SplitFile(path, size);
+                split.Process();
+                Console.WriteLine("Split '{0}' file completed.", path);
+            }
+            else
+            {
+                Console.WriteLine("File '{0}' not found.", path);
+            }
+        }
+
+        /// <summary>
+        /// 在控制台输出参数信息，以便调试之用。
+        /// </summary>
+        /// <param name="args">命令行参数。</param>
+        private static void OutputArgs(IEnumerable<string> args)
+        {
+            foreach (var arg in args)
+            {
+                Console.WriteLine("{0}", arg);
+            }
+        }
+    }
+}
---
+++ pCloudCmd/SplitFile.cs
@@ -0,0 +1,125 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright company="Sunrise" file="SplitFile.cs">
+//   Copyright (c) Sunrise
+//   All rights reserved.
+// </copyright>
+// <summary>
+//   拆分文件处理类。
+// </summary>
+// 
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace PersonalCloud.Command
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// 拆分文件处理类。
+    /// </summary>
+    public class SplitFile
+    {
+        /// <summary>
+        /// 待处理文件路径。
+        /// </summary>
+        private readonly string path;
+
+        /// <summary>
+        /// 拆分成的大小，单位：字节。
+        /// </summary>
+        private readonly long size;
+
+        /// <summary>
+        /// 缓冲区大小。
+        /// </summary>
+        private readonly int bufferSize;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SplitFile"/> class.
+        /// </summary>
+        /// <param name="path">待处理文件路径。</param>
+        /// <param name="size">拆分成的大小，单位：字节。</param>
+        /// <param name="bufferSize">缓冲区大小。</param>
+        public SplitFile(string path, long size, int bufferSize = 4096)
+        {
+            this.path = path;
+            this.size = size;
+            this.bufferSize = bufferSize;
+            CancellationToken = new CancellationToken();
+        }
+
+        /// <summary>
+        /// 获取或设置应取消操作的通知。
+        /// </summary>
+        public CancellationToken CancellationToken { get; set; }
+
+        /// <summary>
+        /// 处理入口。
+        /// </summary>
+        public void Process()
+        {
+            var fileInfo = new FileInfo(this.path);
+            var count = (fileInfo.Length + this.size - 1) / this.size;
+            if (count < 2)
+            {
+                return;
+            }
+
+            var format = new string('0', count.ToString(CultureInfo.InvariantCulture).Length);
+            var files = new List<string>();
+            var tasks = new Task[count];
+            for (var i = 0; i < count && !CancellationToken.IsCancellationRequested; ++i)
+            {
+                var offset = i * this.size;
+                var filePath = this.path + "." + (i + 1).ToString(format);
+                files.Add(filePath);
+                tasks[i] = Task.Run(() => this.Process(offset, filePath), CancellationToken);
+            }
+
+            Task.WaitAll(tasks, CancellationToken);
+
+            if (CancellationToken.IsCancellationRequested)
+            {
+                foreach (var filePath in files.Where(File.Exists))
+                {
+                    File.Delete(filePath);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 处理某段文件区域。
+        /// </summary>
+        /// <param name="offset">读取文件的分段偏移。</param>
+        /// <param name="filePath">写入的文件路径。</param>
+        private void Process(long offset, string filePath)
+        {
+            using (var reader = File.Open(this.path, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                reader.Seek(offset, SeekOrigin.Begin);
+                using (var writer = File.Open(filePath, FileMode.Create, FileAccess.Write, FileShare.None))
+                {
+                    var buffer = new byte[Math.Min(this.bufferSize, this.size)];
+                    var blocks = this.size / buffer.Length;
+                    for (var i = 0; i < blocks && !CancellationToken.IsCancellationRequested; ++i)
+                    {
+                        var count = reader.Read(buffer, 0, buffer.Length);
+                        writer.Write(buffer, 0, count);
+                    }
+
+                    var lastBlock = this.size % buffer.Length;
+                    if (lastBlock != 0)
+                    {
+                        var count = reader.Read(buffer, 0, (int)lastBlock);
+                        writer.Write(buffer, 0, count);
+                    }
+                }
+            }
+        }
+    }
+}
